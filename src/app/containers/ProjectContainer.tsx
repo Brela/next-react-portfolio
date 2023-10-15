b2d5239f4286c840ed0aaa97bafe1f6d06a22f22
Brela <claybreland1@gmail.com>
2023-10-15 12:41:12 -0500

    organized with comments

--- src/app/containers/ProjectContainer.tsx
+++ src/app/containers/ProjectContainer.tsx
@@ -29,18 +29,21 @@ export default function ProjectContainer({
         className="rounded-md min-h-[500px] flex flex-col gap-3 items-center py-3 px-5 lg:px-20"
         style={{ boxShadow: "1px 1px 10px white" }}
       >
-        {/* grouping these to omit flex gap */}
+        {/* ----------------------- title & subtitle ---------------------------------------------------- */}
         <div>
           <h3 className=" text-xl font-semibold text-center">{title}</h3>
           <h4 className="text-accent-green">{subTitle}</h4>
         </div>
+
+        {/* ----------------------- image ---------------------------------------------------- */}
         <figure className="py-2">
           <Image src={screenshot} alt={`${title} Screenshot`} width="533" height="400" />
         </figure>
 
+        {/* ----------------------- details ---------------------------------------------------- */}
         <p>{description}</p>
 
-        {/* Disable buttons if under maintenance */}
+        {/* ----------------------- links ---------------------------------------------------- */}
         <div className="flex gap-4 w-full justify-center px-12 pt-7 rounded-sm">
           <a
             className="underline basis-1/3 rounded-sm text-sm font-semibold py-1 hover:bg-white/30 border border-white/30"
@@ -67,6 +70,7 @@ export default function ProjectContainer({
           </a>
         </div>
 
+        {/* ----------------------- icons ---------------------------------------------------- */}
         <div className="tech-icons flex gap-7 my-5">
           {techIcons.map((icon, index) => (
             <div key={index}>
